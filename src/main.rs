8ef3ef05ce396b8a87ce1acd43d72ba8aec8a898
Dominik Berner <dominik.berner@wingtra.com>
2022-12-23 10:31:27 +0100

    Add candles

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ fn main() {
     println!("termsize {} {}", termsize.0, termsize.1);
     
     // put the top on the tree
-    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 -3), color::Fg(color::Red), "X");
-    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 -2), color::Fg(color::Yellow), "*");
-    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 -1), color::Fg(color::Yellow), "*");
-    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 ), color::Fg(color::Yellow), "*");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 -2, termsize.1 / 2 -4), color::Fg(color::Red), "\\|/");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 -2, termsize.1 / 2 -3), color::Fg(color::Red), "-★-");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 -1, termsize.1 / 2 -2), color::Fg(color::Yellow), "★");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 -1, termsize.1 / 2 -1), color::Fg(color::Yellow), "★");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 -1, termsize.1 / 2 ), color::Fg(color::Yellow), "★");
     
     
     for i in 0..termsize.1 / 2 {
@@ -23,11 +24,23 @@ fn main() {
             tree.push_str("X");
         }
         println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 - i, termsize.1 / 2 + i), color::Fg(color::Green), tree);
+
+        // red balls
         for j in 1..i * 2 
         {
-            if j % 2 == 0 {
-                println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 - j + i -1, termsize.1 / 2 + i), color::Fg(color::Red), "O");
+            
+            if j % 4 == 0 {
+                println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 - j + i , termsize.1 / 2 + i), color::Fg(color::Red), "●");
             }
+        }
+        // candles
+        if i %4 == 1 {
+            println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 - i -2, termsize.1 / 2 + i), color::Fg(color::Yellow), "⅄");
+            println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 + i , termsize.1 / 2 + i), color::Fg(color::Yellow), "⅄");
+        }
+        if i %4 == 2 {
+            println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 - i -1, termsize.1 / 2 + i), color::Fg(color::Red), "‖");
+            println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 + i -1, termsize.1 / 2 + i), color::Fg(color::Red), "‖");
         }
     }
     print!("{}", style::Reset);
