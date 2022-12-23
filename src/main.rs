93587bb4929bcf9b038dec708c341abe27e20310
Dominik Berner <dominik.berner@wingtra.com>
2022-12-23 09:57:58 +0100

    Add Balls to tree

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,28 @@ fn main() {
     //println!("{}Hello, world!{}", color::Fg(color::Green), style::Reset);
     let termsize = termion::terminal_size().unwrap();
     println!("termsize {} {}", termsize.0, termsize.1);
-    // println!("{}{}{}", termion::cursor::Goto(termsize.0 /2, termsize.1 / 2), color::Fg(color::Green), "X");
-    // println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 - 1, termsize.1 / 2 + 1), color::Fg(color::Green), "YYY");
-    // println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 - 2, termsize.1 / 2 + 2), color::Fg(color::Green), "ZZZZZ");
-    // println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 - 3, termsize.1 / 2 + 3), color::Fg(color::Green), "1111111");
-    let x_top = termsize.0 / 2;
-    for i in 0..termsize.1 - 2 {
+    
+    // put the top on the tree
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 -3), color::Fg(color::Red), "X");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 -2), color::Fg(color::Yellow), "*");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 -1), color::Fg(color::Yellow), "*");
+    println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 -1, termsize.1 / 2 ), color::Fg(color::Yellow), "*");
+    
+    
+    for i in 0..termsize.1 / 2 {
         let mut tree = String::new();
-        for j in 1..i * 2
+        // green tree
+        for _j in 1..i * 2
         {
             tree.push_str("X");
         }
         println!("{}{}{}", termion::cursor::Goto(termsize.0 /2 - i, termsize.1 / 2 + i), color::Fg(color::Green), tree);
-
+        for j in 1..i * 2 
+        {
+            if j % 2 == 0 {
+                println!("{}{}{}", termion::cursor::Goto(termsize.0 / 2 - j + i -1, termsize.1 / 2 + i), color::Fg(color::Red), "O");
+            }
+        }
     }
     print!("{}", style::Reset);
 
